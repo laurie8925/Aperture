1130d62d9025a98353db50d8f30f5bae5d16d764
unknown <laurie8925@gmail.com>
2025-03-17 20:29:58 -0700

    added photo entry file

---
+++ aperture/components/PhotoEntryScreen.tsx
@@ -0,0 +1,12 @@
+import { useState } from 'react';
+import { Image, View, StyleSheet,Text } from 'react-native';
+import { Button, Input } from '@rneui/themed';
+import * as ImagePicker from 'expo-image-picker';
+
+export default function PhotoEntryScreen() {
+  return (
+    <View>
+      <Text>PhotoEntryScreen</Text>
+    </View>
+  )
+}
